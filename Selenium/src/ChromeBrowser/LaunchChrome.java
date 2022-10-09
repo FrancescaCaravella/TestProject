d9ecea7adc04ad3c870c85ef43d0f1cd136eab95
franc <franc@DESKTOP-BMG0L88>
2022-10-09 19:45:02 +0200

    Third Commit

--- Selenium/src/ChromeBrowser/LaunchChrome.java
+++ Selenium/src/ChromeBrowser/LaunchChrome.java
@@ -5,14 +5,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchChrome {
 	public static void main() {
-		
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\franc\\Downloads\\chromedriver.exe");
-			
+
 		WebDriverManager.chromedriver().setup();
-		
+
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
-		
+
 	}
 
 }
